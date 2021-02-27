2d3b7f6bbd6e1ee27556d8e546fa161ae3a67df1
Pranav Patki <66993183+pranav0609@users.noreply.github.com>
2021-02-27 20:41:52 +0530

    map styling

--- vaccine-locator/src/Components/Mapp.js
+++ vaccine-locator/src/Components/Mapp.js
@@ -1,22 +1,39 @@
 import React, { useState } from 'react'
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, {Marker} from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
-
+import './mapstylesheet.css'
+import * as ParkData from "./skateboard-parks.json"
 function Mapp() {
     const [viewport, setViewport] = useState({
-        width: window.innerWidth,
-        height: window.innerHeight,
+        
         latitude: 37.7577,
         longitude: -122.4376,
+        width: "100vw",
+        height: "100vh",
         zoom: 8
     });
 
     return (
-        <ReactMapGL
-            mapboxApiAccessToken={"pk.eyJ1IjoiYmhhdmlucDE2IiwiYSI6ImNrbG5vcWswdDBsNGwydW4zcHd2cjl2aWsifQ.RcqH82Tfx9LNpsLyaPP68g"}
-            {...viewport}
-            onViewportChange={(newView) => setViewport(newView)}
-        />
+        <div>
+            <ReactMapGL {...viewport}
+            mapboxApiAccessToken="pk.eyJ1IjoicHJhbmF2cGF0a2kiLCJhIjoiY2tsbm54bWFiMDM0bDJ3cGg0aDdnb2I5bSJ9.Wb2YJ9VaMxy7CLM0eYv_FQ"
+            mapStyle="mapbox://styles/pranavpatki/cklnskzam0fra17tm7n4q8d7f"
+            onViewportChange={viewport => {
+                setViewport(viewport);
+            }}
+            >
+                {ParkData.features.map((park)=> (
+                    <Marker
+                    key={park.properties.PARK_ID}
+                    latitude={park.geometry.coordinates[1]}
+                    longitude={park.geometry.coordinates[0]}>
+                        <button>
+                            <img src="/304919.svg" alt="hospital icon" />
+                        </button>
+                    </Marker>
+                ))}
+            </ReactMapGL>
+        </div>
     );
 }
 export default Mapp
