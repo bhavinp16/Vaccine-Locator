81e3447634af9f0d2dce9283344c80e059690396
Bhavin <bhavin1625p@gmail.com>
2021-02-27 22:08:11 +0530

    pin

--- vaccine-locator/src/Components/Mapp.js
+++ vaccine-locator/src/Components/Mapp.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
-import ReactMapGL, {Marker} from 'react-map-gl';
+import ReactMapGL, { Marker } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './mapstylesheet.css'
 import * as ParkData from "./skateboard-parks.json"
 function Mapp() {
     const [viewport, setViewport] = useState({
-        
+
         latitude: 37.7577,
         longitude: -122.4376,
         width: "100vw",
@@ -16,19 +16,19 @@ function Mapp() {
     return (
         <div>
             <ReactMapGL {...viewport}
-            mapboxApiAccessToken="pk.eyJ1IjoicHJhbmF2cGF0a2kiLCJhIjoiY2tsbm54bWFiMDM0bDJ3cGg0aDdnb2I5bSJ9.Wb2YJ9VaMxy7CLM0eYv_FQ"
-            mapStyle="mapbox://styles/pranavpatki/cklnskzam0fra17tm7n4q8d7f"
-            onViewportChange={viewport => {
-                setViewport(viewport);
-            }}
+                mapboxApiAccessToken="pk.eyJ1IjoicHJhbmF2cGF0a2kiLCJhIjoiY2tsbm54bWFiMDM0bDJ3cGg0aDdnb2I5bSJ9.Wb2YJ9VaMxy7CLM0eYv_FQ"
+                mapStyle="mapbox://styles/mapbox/dark-v9"
+                onViewportChange={viewport => {
+                    setViewport(viewport);
+                }}
             >
-                {ParkData.features.map((park)=> (
+                {ParkData.features.map((park) => (
                     <Marker
-                    key={park.properties.PARK_ID}
-                    latitude={park.geometry.coordinates[1]}
-                    longitude={park.geometry.coordinates[0]}>
+                        key={park.properties.PARK_ID}
+                        latitude={park.geometry.coordinates[1]}
+                        longitude={park.geometry.coordinates[0]}>
                         <button>
-                            <img src="/304919.svg" alt="hospital icon" />
+                            <img className="pin" src="https://www.pngrepo.com/png/90074/512/hospital.png" alt="#" />
                         </button>
                     </Marker>
                 ))}
